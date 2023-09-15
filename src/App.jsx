a7e958589762257f2952ac73d3f6083248620b60
mryaxyobek <pubgn9642@gmail.com>
2023-09-15 21:24:40 +0500

    initial files and folders are configured

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+// import { NavLink, Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    // <div>
+    //   <header className="bg-blue-100 text-blue-600 py-1">
+    //     <div className="container flex items-center justify-between">
+    //       <a href="#" className='font-bold text-5xl'>Logo</a>
+    //       <ul className="flex items-center">
+    //         <li>
+    //           <NavLink className='py-5 px-5' to='home'>Home</NavLink>
+    //         </li>
+    //         <li>
+    //           <NavLink className='py-5 px-5' to='about'>About</NavLink>
+    //         </li>
+    //         <li>
+    //           <NavLink className='py-5 px-5' to='contact'>Contact</NavLink>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </header>
+
+    //   <main>  
+    //     <div className="container">
+    //       <Routes>
+    //         <Route path='/' element={<Home/>} />
+    //         <Route path='/about' element={<About/>} />
+    //         <Route path='/contact' element={<Contact/>} />
+    //       </Routes>
+    //     </div>
+    //   </main>
+    // </div>
+    <>
+      app
+    </>
+  )
+}
+
+export default App
