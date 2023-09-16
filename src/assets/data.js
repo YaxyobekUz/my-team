037d8934e60394f5998ddf24511e464deb14d48b
mryaxyobek <pubgn9642@gmail.com>
2023-09-16 22:36:22 +0500

    added home page about people data at data.js

---
+++ src/assets/data.js
@@ -0,0 +1,26 @@
+import kadyImg from './images/kady.png';
+import aishaImg from './images/aisha.png';
+import arthurImg from './images/arthur.png';
+export const homePageAboutPeople = [
+    {
+        id: 1,
+        name: 'Kady Baker',
+        description: ' “The team perfectly fit the specialized skill set required. They focused on the most essential features helping us launch the platform eight months faster than planned.”',
+        job: 'Product Manager at Bookmark',
+        img: kadyImg,
+    },
+    {
+        id: 2,
+        name: 'Aiysha Reese',
+        description: '“We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!”',
+        job: 'Founder of Manage',
+        img: aishaImg,
+    },
+    {
+        id: 3,
+        name: 'Arthur Clarke',
+        description: '“Amazing. Our team helped us build an app that delivered a new experience for hiring a physio. The launch was an instant success with 100k downloads in the first month.”',
+        job: 'Co-founder of MyPhysio',
+        img: arthurImg,
+    },
+]
