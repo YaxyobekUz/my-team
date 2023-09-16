9ed4db690b9fd528a10a3e799f02173a940b8d73
mryaxyobek <pubgn9642@gmail.com>
2023-09-16 12:26:18 +0500

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../assets/images/myteam-logo.svg';
 
 const Header = () => {
   return (
     <header className='text-white py-73'>
-      header
+      <div className="flex container items-center justify-between">
+        {/* logo  */}
+        <Link to='/'>
+          <img className='mr-20' width={160} height={40} src={Logo} alt="my team logo, header logo" />
+        </Link>
+        {/* navbar menu  */}
+        <nav className='flex items-center justify-between w-full leading-7'>
+          <ul className='flex items-center space-x-10'>
+            <li>
+              <NavLink className={'hover:text-primary-light_coral transition-colors'} to='/'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink className={'hover:text-primary-light_coral transition-colors'} to='about'>About</NavLink>
+            </li>
+          </ul>
+          <NavLink className={'primary-btn_light hover:primary-btn_light-hover transition-colors'} to='contact'>contact us</NavLink>
+        </nav>
+      </div>
     </header>
   )
 }
