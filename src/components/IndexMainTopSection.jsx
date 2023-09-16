7c6988e3ea7b46f4489dcee61ee9ee11221a5a4c
mryaxyobek <pubgn9642@gmail.com>
2023-09-16 16:57:07 +0500

    added homepage maintopsection.jsx file

---
+++ src/components/IndexMainTopSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexMainTopSection = () => {
+  return (
+    <section>IndexMainTopSection</section>
+  )
+}
+
+export default IndexMainTopSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/IndexHero'
+import IndexMainTopSection from '../components/IndexMainTopSection'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <IndexMainTopSection />
     </div>
   )
 }
