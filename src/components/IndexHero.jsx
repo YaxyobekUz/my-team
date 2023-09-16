5bd49cb674738928728999bfba45d6146b99e28f
mryaxyobek <pubgn9642@gmail.com>
2023-09-16 17:59:31 +0500

    added Index Main Top Section

--- src/components/IndexHero.jsx
+++ src/components/IndexHero.jsx
@@ -4,7 +4,7 @@ import circleShapes from '../assets/images/circle-shapes.svg'
 
 const IndexHero = () => {
     return (
-        <div className='pt-60 pb-250 z-1'>
+        <div className='pt-60 pb-250'>
             <div className="flex items-center container relative">
                 <h1 className="text-100 max-w-635 w-full leading-100 font-bold mr-30">Find the <br /> best <span className="text-primary-light_coral">talent</span></h1>
                 <div>
@@ -12,7 +12,7 @@ const IndexHero = () => {
                     <p className="max-w-445">Finding the right people and building high performing teams can be hard. Most companies aren’t tapping into the abundance of global talent. We’re about to change that.</p>
                 </div>
                 <img width={200} height={200} className='absolute -left-265' src={circleShape} alt="" aria-hidden='true' />
-                <img width={358} height={200} className='absolute -bottom-350 right-5' src={circleShapes} alt="" aria-hidden='true' />
+                <img width={358} height={100} className='flex items-start absolute -bottom-250 right-5 object-cover object-top h-1/2' src={circleShapes} alt="" aria-hidden='true' />
             </div>
         </div>
     )
--- src/components/IndexMainTopSection.jsx
+++ src/components/IndexMainTopSection.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import boyImg from '../assets/images/boy-icon.svg';
+import parameterImg from '../assets/images/parameter-icon.svg';
+import statisticsImg from '../assets/images/statistics-icon.svg';
+import shapeImg from '../assets/images/circle-shape1.svg';
 
 const IndexMainTopSection = () => {
-  return (
-    <section>IndexMainTopSection</section>
-  )
+    return (
+        <section className='bg-secondary-sacramento_state_green py-140'>
+            <div className="grid container grid-cols-2 gap-5 justify-between items-start relative">
+                <div>
+                    <div className="restangle-shape mb-52"></div>
+                    <h2 className="max-w-445">Build & manage distributed teams like no one else.</h2>
+                </div>
+                <ul className="space-y-8 mt-52">
+                    <li className="flex items-start">
+                        <img width={72} height={72} src={boyImg} alt="boy icon" className="mr-6" />
+                        <div className='max-w-445'>
+                            <h3 className="text-primary-light_coral mb-4">Experienced Individuals</h3>
+                            <p className="small-p opacity-8">Our network is made up of highly experienced professionals who are passionate about what they do.</p>
+                        </div>
+                    </li>
+                    <li className="flex items-start">
+                        <img width={72} height={72} src={parameterImg} alt="parameter icon" className="mr-6" />
+                        <div className='max-w-445'>
+                            <h3 className="text-primary-light_coral mb-4">Easy to Implement</h3>
+                            <p className="small-p opacity-8">Our processes have been refined over years of implementation meaning our teams always deliver.</p>
+                        </div>
+                    </li>
+                    <li className="flex items-start">
+                        <img width={72} height={72} src={statisticsImg} alt="statistics icon" className="mr-6" />
+                        <div className='max-w-445'>
+                            <h3 className="text-primary-light_coral mb-4">Enhanced Productivity</h3>
+                            <p className="small-p opacity-8">Our customized platform with in-built analytics helps you manage your distributed teams.</p>
+                        </div>
+                    </li>
+                </ul>
+                <img width={200} height={244} className='absolute -right-265 -bottom-140' src={shapeImg} alt="" aria-hidden='true' />
+            </div>
+        </section>
+    )
 }
 
 export default IndexMainTopSection
