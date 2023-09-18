71d07f1f26c9d0c4573e92e6ec0de017e80e0c4b
mryaxyobek <pubgn9642@gmail.com>
2023-09-18 12:07:57 +0500

    about page clients images bug fixed

--- src/components/AboutPageClientsInfo.jsx
+++ src/components/AboutPageClientsInfo.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const AboutPageClientsInfo = () => {
     return (
-        <section className='py-140 bg-secondary-deep_jungle_green'>
+        <section className='py-140 bg-secondary-sacramento_state_green'>
             <div className="container">
-                <h2 className="text-center">Some of our clients</h2>
+                <h2 className="text-center mb-16 max-w-917">Some of our clients</h2>
             </div>
         </section>
     )
